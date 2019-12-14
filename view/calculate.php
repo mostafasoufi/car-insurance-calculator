8e70d27610fd8f40d7b9099c5466029b4caa47d2
Mostafa Soufi <mostafa.soufi@hotmail.com>
2019-12-14 13:22:14 +0200

    Complete the calculate html form.

--- view/calculate.php
+++ view/calculate.php
@@ -8,6 +8,62 @@
                 </div>
             </div>
         </div>
+
+        <hr />
+
+        <div class="row">
+            <div class="col-md-6">
+                <form method="post" action="">
+                    <div class="form-group row">
+                        <label for="car-value" class="col-sm-6 col-form-label">Estimated value of the car</label>
+                        <div class="col-sm-6">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">€</span>
+                                </div>
+                                <input type="number" name="car-value" class="form-control" id="car-value" placeholder="" min="1" required>
+                            </div>
+
+                            <small id="passwordHelpBlock" class="form-text text-muted">
+                                100 - 100 000 EUR
+                            </small>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="tax-percentage" class="col-sm-6 col-form-label">Tax percentage</label>
+                        <div class="col-sm-6">
+                            <div class="input-group">
+                                <input type="number" name="tax-percentage" class="form-control" id="tax-percentage" placeholder="" min="0" required>
+                                <div class="input-group-append">
+                                    <span class="input-group-text">%</span>
+                                </div>
+                            </div>
+                            
+                            <small id="passwordHelpBlock" class="form-text text-muted">
+                                0 - 100%
+                            </small>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="instalments" class="col-sm-6 col-form-label">Number of instalments</label>
+                        <div class="col-sm-6">
+                            <input type="number" class="form-control" id="instalments" placeholder="" min="0" max="12" required>
+                            <small id="passwordHelpBlock" class="form-text text-muted">
+                                count of payments in which client wants to pay for the policy (1 – 12)
+                            </small>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary">Calculate</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 
 <?php include('footer.php'); ?>
--- view/header.php
+++ view/header.php
@@ -36,10 +36,6 @@
 
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="/">Home</a>
-            </li>
-
             <li class="nav-item">
                 <a class="nav-link" href="calculator">Calculate</a>
             </li>
